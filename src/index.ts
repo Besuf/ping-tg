cc246cdc876ec1dc191b0a70ade0e22c08951024
Besufekad <besufkeat@gmail.com>
2025-04-06 19:48:25 +0300

    feat: update .env.example and index.ts to include TELEGRAM_CHAT_ID for message sending

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { sendMessage } from './actions/sendMessage';
 
+const myChatId = process.env.TELEGRAM_CHAT_ID || '';
+
 const main = async () => {
-  const chatId = '123456789'; // Replace with the actual chat ID
-  const text = 'Hello, this is a test message!';
+  const text =
+    'Hello, how are you? This is a test message from my Telegram bot!';
 
   try {
-    const response = await sendMessage(chatId, text);
+    console.log('chatId', myChatId);
+    const response = await sendMessage(myChatId, text);
     console.log('Message sent successfully:', response);
   } catch (error) {
     console.error('Error sending message:', error);
